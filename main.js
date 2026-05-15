888898d88b2caec168515a86bdbe6af32eda5401
thomaslazzeri3 <thomaslazzeri3@gmail.com>
2026-05-15 20:26:54 +0200

    1.7 arrays exercise done

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ teachers.push('Vanessa');
 teachers.unshift('Sarah');
 // 7. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
 // e salvalo nella variabile lewisIndex
-const lewisIndex = null;
+const lewisIndex = teachers.indexOf('Lewis');
 
 // 8. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
