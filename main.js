8637bb86ca3e952a5844ce2defa25d034036af24
thomaslazzeri3 <thomaslazzeri3@gmail.com>
2026-05-15 20:15:28 +0200

    1.2 arrays exercise done

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ const teachers = [
 const fourthTeacher = teachers[3];
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
-
+teachers[4] = 'Patrick';
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
 const lastTeacher = null;
 
