3f12f63b3b748eabdd3a61078ef12408dde85633
thomaslazzeri3 <thomaslazzeri3@gmail.com>
2026-05-24 15:36:02 +0200

    changed hmtl and changed exercise 2 in for

--- 02-arrays-bonus.js
+++ 02-arrays-bonus.js
@@ -10,17 +10,65 @@ const teachers = [
 
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
-const reversedTeachers = teachers.reverse();
+// const reversedTeachers = teachers.reverse();
+
+const reversedTeachers = [];
+
+for (let i = teachers.length - 1; i >= 0; i--) {
+  reversedTeachers.push(teachers[i]);
+}
+console.log(reversedTeachers);
+
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = teachers.filter(teacher => teacher.length >= 5);
+// const longNames = teachers.filter(teacher => teacher.length >= 5);
+
+const longNames = [];
+
+for (let i = 0; i < teachers.length; i++) {
+  if (teachers[i].length >= 5) {
+    longNames.push(teachers[i]);
+  }
+}
+console.log(longNames);
 
 // 3. Rimuovi 'Ed' dall'array teachers
-teachers.splice(teachers.indexOf('Ed'), 1);
+// teachers.splice(teachers.indexOf('Ed'), 1);
+
+const teachersCopy2 = [...teachers];
+
+for (let i = 0; i < teachersCopy2.length; i++) {
+  if (teachersCopy2[i] === 'Ed') {
+    teachersCopy2.splice(i, 1);
+    break;
+  }
+}
+console.log("Es 3 (senza Ed):", teachersCopy2);
+
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
-const isFabioPresent = teachers.includes('Fabio');
+// const isFabioPresent = teachers.includes('Fabio');
+
+let isFabioPresent = false;
+
+for (let i = 0; i < teachers.length; i++) {
+  if (teachers[i] === 'Fabio') {
+    isFabioPresent = true;
+    break;
+  }
+}
+console.log(isFabioPresent);
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
-const teachersString = teachers.join(', ');
+// const teachersString = teachers.join(', ');
+
+let teachersString = '';
+
+for (let i = 0; i < teachers.length; i++) {
+  teachersString += teachers[i];
+  if (i < teachers.length - 1) {
+    teachersString += ', ';
+  }
+}
+console.log(teachersString);
